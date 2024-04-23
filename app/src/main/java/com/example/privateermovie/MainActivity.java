51f4e350575909ea378a824e42a0767efd1deaac
KasperFlodin <Kasperflodin@gmail.com>
2024-04-23 14:12:10 +0200

    3nd commit

--- app/src/main/java/com/example/privateermovie/MainActivity.java
+++ app/src/main/java/com/example/privateermovie/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.privateermovie;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    RequestQueue rq;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,22 +37,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initGui();
         fragmentChanger(StartFragment.class);
+        rq = Volley.newRequestQueue(getApplicationContext());
+        getMovieBySearch("Star Wars");
+        //getMovieByIdAsync(17);
     }
 
     void  initGui(){
-        findViewById(R.id.btn_movies).setOnClickListener(v -> {
-
-        });
-        findViewById(R.id.btn_series).setOnClickListener(v -> {
-
-        });
-        findViewById(R.id.btn_watchlist).setOnClickListener(v -> {
-
-        });
-        findViewById(R.id.btn_search).setOnClickListener(v -> {
-
-        });
+        findViewById(R.id.nav_movie).setOnClickListener(v -> {fragmentChanger(MovieFragment.class);});
+        findViewById(R.id.nav_series).setOnClickListener(v -> {fragmentChanger(SeriesFragment.class);});
+        findViewById(R.id.nav_watchlist).setOnClickListener(v -> {fragmentChanger(WatchlistFragment.class);});
+        findViewById(R.id.btn_search).setOnClickListener(v -> {});
     }
 
 
@@ -49,5 +60,75 @@ public class MainActivity extends AppCompatActivity {
                 .addToBackStack("name") // Name can be null
                 .commit();
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.getBackStackEntryCount() > 0) {
+            fragmentManager.popBackStack();
+        } else {
+            showCloseAppDialog();
+        }
+    }
+
+    private void showCloseAppDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage("Going back further will close the app. Are you sure you want to continue?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Close the app
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Dismiss the dialog
+                        dialogInterface.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    public void getMovieByIdAsync(int id) {
+        RequestQueue rq = Volley.newRequestQueue(getApplicationContext());
+        {
+            String url = "https://api.themoviedb.org/3/movie/" + id;
+            StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
+                Movie movie = new Gson().fromJson(response, Movie.class);
+                Toast.makeText(getApplicationContext(), movie.title, Toast.LENGTH_LONG).show();
+            }, error -> Log.e("Volley", error.toString()))
+            {
+                @Override
+                public Map<String, String> getHeaders(){
+                    Map<String, String>  params = new HashMap<String, String>();
+                    params.put("Authorization", secrets.Token);
+                    return params;
+                }
+            };
+            rq.add(request);
+        }
+    }
+
+    void getMovieBySearch(String query)       {
+        String url = "https://api.themoviedb.org/3/search/movie?query=" + query;
+        StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
+            MovieSearch movieSearch = new Gson().fromJson(response, MovieSearch.class);
+            Toast.makeText(getApplicationContext(), movieSearch.total_results, Toast.LENGTH_LONG).show();
+        }, error -> Log.e("Volley", error.toString()))
+        {
+            @Override
+            public Map<String, String> getHeaders(){
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Authorization", secrets.Token);
+                return params;
+            }
+        };
+        rq.add(request);
+    }
+
+
 
 }
